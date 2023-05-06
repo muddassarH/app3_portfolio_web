0762d5e98c74e93182754ca7f44a2fb6bf8de890
muddassarH <muddassarhussain90@gmail.com>
2023-05-06 22:43:21 +0500

    day 22: render the images and urls

--- main.py
+++ main.py
@@ -39,17 +39,21 @@ Below You can find the app i have built in python .Feel Free to Contact Me:
 st.write(content, font=20)
 
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
-        st.info(row['description'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f"[Source Code]({row['url']})")
 
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row['title'])
-        st.info(row['description'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f"[Source Code]({row['url']})")
